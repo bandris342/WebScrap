9643489553ca31c7f8c689abfc57176be2191b16
bandris342 <andras.bartok@gmail.com>
2017-12-23 23:52:05 +0100

    Local PostgreSQL Database connection

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Web-scraping exercise for AF
 ## Using a Twitter account
 ## Loop until keyboard interruption
+## Save data to a local PostgreSQL Table
--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-import requests, time
+import requests, time, psycopg2, sys
 from lxml import html
 
 try:
+    conn = psycopg2.connect("dbname='WebScrap' user='postgres' host='localhost' password='kortefa'")
+except:
+    print("I am unable to connect to the database!")
+    sys.exit(1)
+
+cur = conn.cursor()
+try:
     while True:
         pagestring = requests.get('https://twitter.com/emmanuelmacron')
-        CurrTime=time.localtime()
+        CurrTime=int(time.time())
         htmlpage = html.fromstring(pagestring.content)
         followers = int(htmlpage.xpath("//li[@class='ProfileNav-item ProfileNav-item--followers']//@data-count")[0])
-        print(time.strftime("%Y-%m-%d %H:%M:%S", CurrTime), followers)
+        cur.execute("INSERT INTO followers (time, count) VALUES (%s, %s);", (CurrTime, followers))
+        conn.commit()
+        print(time.ctime(CurrTime), followers)
         time.sleep(2)
 except KeyboardInterrupt:
+    cur.close()
+    conn.close()
     pass
