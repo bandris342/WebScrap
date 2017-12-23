0564d1868c29c85fa344bebfaa720fa901c588ba
bandris342 <andras.bartok@gmail.com>
2017-12-23 11:21:05 +0100

    Loop until keyboard interruption

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Web-scraping exercise for AF
 ## Using a Twitter account
-
+## Loop until keyboard interruption
--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-import requests
+import requests, time
 from lxml import html
-pagestring = requests.get('https://twitter.com/emmanuelmacron')
-htmlpage = html.fromstring(pagestring.content)
-followers = int(htmlpage.xpath("//li[@class='ProfileNav-item ProfileNav-item--followers']//@data-count")[0])
-print(followers)
+
+try:
+    while True:
+        pagestring = requests.get('https://twitter.com/emmanuelmacron')
+        CurrTime=time.localtime()
+        htmlpage = html.fromstring(pagestring.content)
+        followers = int(htmlpage.xpath("//li[@class='ProfileNav-item ProfileNav-item--followers']//@data-count")[0])
+        print(time.strftime("%Y-%m-%d %H:%M:%S", CurrTime), followers)
+        time.sleep(2)
+except KeyboardInterrupt:
+    pass
