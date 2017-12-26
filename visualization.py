c64eed6e2bec99200044325bab64a70f034b2266
bandris342 <andras.bartok@gmail.com>
2017-12-27 00:21:23 +0100

    A simple data visualization script

--- visualization.py
+++ visualization.py
@@ -1,4 +1,10 @@
 import psycopg2, time, sys
+import matplotlib.pyplot as plt
+import numpy
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
 
 #try to connect to the database
 try:
@@ -17,12 +23,55 @@ except:
     print("Followers table not exists!")
     sys.exit(1)
 
-CurrTime = int(time.time())
-cur.execute("SELECT * FROM followers WHERE time > %s ORDER BY time", (CurrTime-60,))
-data=cur.fetchall()
+cur.execute("SELECT COUNT(*) FROM followers;")
+NbRows=cur.fetchone()
 
-for row in data:
-    print(time.ctime(row[0]), row[1])
+cur.execute("SELECT time FROM followers ORDER BY time ASC LIMIT 1")
+FirstTime=cur.fetchone()
+
+cur.execute("SELECT time FROM followers ORDER BY time DESC LIMIT 1")
+LastTime=cur.fetchone()
+
+print("There are %d rows in the database. Data have been collected between %s and %s" % (NbRows[0], time.ctime(FirstTime[0]), time.ctime(LastTime[0])))
+
+Tback=int(input("Give me a time in minutes (dTime) to visualize data collected between T-dTime and T:"))*60
+
+cur.execute("SELECT * FROM followers WHERE time > %s ORDER BY time", [LastTime[0] - Tback])
+result=cur.fetchall()
 
 cur.close()
 conn.close()
+
+
+#Data plot
+
+data = []
+xTickMarks = []
+
+for row in result:
+   data.append(int(row[1]))
+   xTickMarks.append(time.strftime("%D %H:%M:%S", time.localtime(row[0])))
+
+# necessary variables
+ind = numpy.arange(len(data))     # the x locations for XTickers
+width = 0.35                      # the width of the bars
+
+#plot the bars
+ax.bar(ind, data, width, color='black')
+
+
+#set axes and labels
+ax.set_xlim(-width,len(ind)+width)
+ax.set_ylim(min(data)-1, max(data)+1)
+
+
+ax.set_ylabel('Number of followers')
+ax.set_xlabel('Data number')
+ax.set_title('Data collected between \n %s and %s\n' % (xTickMarks[0], xTickMarks[len(data)-1]))
+
+#ax.set_xticks(ind+width)
+#xtickNames = ax.set_xticklabels(xTickMarks)
+#plt.setp(xtickNames, rotation=45, fontsize=10)
+
+plt.show()
+
